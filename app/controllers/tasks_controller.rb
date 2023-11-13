c4190423cca9704c2e8137c564aa20a081723b16
ThierryAra <thierryara@usp.br>
2023-11-13 14:41:56 -0300

    Create task_controller delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,9 +15,16 @@ class TasksController < ApplicationController
         redirect_to root_path 
       end
   
-      private
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+
+        redirect_back(fallback_location: root_path)
+    end
     
-      def task_params
+    private
+
+    def task_params
         params.require(:task).permit(:title, :note, :completed, :list_id)
-      end
+    end
 end
