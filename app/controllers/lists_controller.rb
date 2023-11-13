b7f3a636d214553fa97433124d27773850e8d713
ThierryAra <thierryara@usp.br>
2023-11-13 13:24:56 -0300

    Change router new_task_and_show to new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,18 @@
 class ListsController < ApplicationController
+    before_action :set_list
+
     def new
-        @list = List.new
+        @list = List.create(title: 'New list', description: 'description')
+        
+        redirect_back(fallback_location: root_path)
     end
 
     def show
-        @lists = List.includes(:tasks).all
         redirect_back(fallback_location: root_path)
+    end
+
+    private
+    def set_list
+        @lists = List.includes(:tasks).all
     end
 end
