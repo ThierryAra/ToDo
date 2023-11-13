87c8fb9ee5be04ee7d4a6cbcd5e3e2eaa359b59a
ThierryAra <thierryara@usp.br>
2023-11-13 14:37:31 -0300

    Create task create logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,21 @@ class TasksController < ApplicationController
         @task = Task.new
     end
 
+    def create
+        @task = Task.new(task_params)
     
+        if @task.save
+          flash[:success] = 'Task created successfully!'
+        else
+            flash[:error] = 'Failed to create task.'
+        end
+        
+        redirect_to root_path 
+      end
+  
+      private
+    
+      def task_params
+        params.require(:task).permit(:title, :note, :completed, :list_id)
+      end
 end
