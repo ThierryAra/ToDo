9d25fb9ac8b275486e1a93140abb69f06019781b
ThierryAra <thierryara@usp.br>
2023-12-04 16:03:54 -0300

    Add default formatting to controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,40 +1,56 @@
 class ListsController < ApplicationController
     before_action :set_list
+    after_action :set_session
 
     def new
         @list = List.create(title: 'New list', description: 'description')
-        
-        redirect_to root_path
-    end
 
-    def show
+        if @list.save
+            flash[:success] = 'List created successfully!'
+            session[:reload_root] = true
+            session[:flash_message] = flash.to_hash
+        else
+            flash[:error]  = 'Failed to create list.'
+        end
+    
         redirect_to root_path
     end
 
     def destroy
         @list = List.find(params[:id])
         @list.destroy
-    
-        respond_to do |format|
-          format.html { redirect_to root_path, notice: 'Lista excluída com sucesso.' }
-          format.json { head :no_content }
+
+        if @list.destroy
+            flash[:success] = 'List deleted successfully!'
+        else
+            flash[:error]  = 'Failed to delete list.'
         end
+        
+        session[:reload_root] = true
+        session[:flash_message] = flash.to_hash
+        redirect_to root_path
     end
 
     def update
         @list = List.find(params[:id])
         if @list.update(list_params)
-          render json: { success: true, message: 'List updated successfully' }
-            else
-          render json: { success: false, message: 'Error updating list' }
+            render json: { success: true, message: 'List updated successfully' }
+        else
+            render json: { success: false, message: 'Error updating list' }
         end
     end
-
+    
+    private
     def list_params
         params.require(:list).permit(:title, :description)
     end 
-    private
+
     def set_list
         @lists = List.includes(:tasks).all
+    end
+
+    def set_session
+        session[:reload_root] = true
+        session[:flash_message] = flash.to_hash
     end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,28 @@
 class TasksController < ApplicationController
-    def new
-        @task = Task.new
-    end
-
+    after_action :set_session
+ 
     def create
         @task = Task.new(task_params)
-    
+
         if @task.save
-          flash[:success] = 'Task created successfully!'
-          else
+            flash[:success] = 'Task created successfully!'
+        else
             flash[:error] = 'Failed to create task.'
         end
         
-        redirect_to root_path 
-      end
+        redirect_to root_path
+    end
   
     def destroy
         @task = Task.find(params[:id])
         
         if @task.destroy
             flash[:success] = 'Task deleted successfully!'
-            else
-                flash[:error] = 'Failed to delete task.'
+        else
+            flash[:error] = 'Failed to delete task.'
         end
-
-        redirect_back(fallback_location: root_path)
+        
+        redirect_to root_path
     end
 
     def update
@@ -32,16 +30,21 @@ class TasksController < ApplicationController
         
         if @task.update(task_params)
             flash[:success] = 'Task updated successfully!'
-            else
-                flash[:error] = 'Failed to update task.'
+        else
+            flash[:error] = 'Failed to update task.'
         end
 
-        redirect_back(fallback_location: root_path)
+        redirect_to root_path
     end
     
     private
 
     def task_params
         params.require(:task).permit(:title, :note, :completed, :list_id)
+    end
+
+    def set_session
+        session[:reload_root] = true
+        session[:flash_message] = flash.to_hash
     end
 end
